0cbfc246a7ead0d21111e86b790e725e6d16a2a4
Gabriel Kutsovsky <gkutsovsky@gmail.com>
2019-02-16 12:24:23 -0500

    first commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+const Home = () => {
+  return(
+    <div>
+      <Navbar/>
+      <h1>This is the Home Page</h1>
+    </div>
+  )
+}
+
+export default Home
