3ff68d178ffda7bdeb5aa705fa99a0a1e4a5507e
Hope Hutman <hhutman@telefonprojekt.com>
2019-02-17 16:30:47 -0500

    add default pictures and lineage

--- src/Pages/Pairings.js
+++ src/Pages/Pairings.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
+function chooseImage(currentItem) {
+  if (currentItem.image === "https://www.cannabisreports.com/images/strains/no_image.png") {
+    return "https://magazine.grasscity.com/wp-content/uploads/2016/02/How-To-Harvest-Outdoor-Marijuana-Plants-720x480@2x.jpg";
+  }
+  else {
+    return currentItem.image;
+  }
+}
+
 const Pairings = ({ currentItem }) => {
   return(
     <div>
-      {console.log(currentItem)}
-      <h1>{currentItem.name}</h1>
-      <img src={currentItem.image} />
+      <img src={chooseImage(currentItem)} />
+      <h3>{currentItem.name}</h3>
+      <div class = "lineage">
+      <h3>Parental Lineage</h3>
+      {currentItem.lineage && Object.keys(currentItem.lineage).map(key => <div>{key}</div>)}
+      </div>
+
+
     </div>
   )
 }
