13d079e5db448cf286b1d5388a30cd87e63ee1a8
Hope Hutman <hhutman@telefonprojekt.com>
2019-02-18 12:21:00 -0500

    added beautyshots

--- src/Pages/Beautyshots.js
+++ src/Pages/Beautyshots.js
@@ -1,5 +1,8 @@
-const description = ["https://www.southernladymagazine.com/wp-content/uploads/2015/05/Picnic1.jpg", "https://i2.wp.com/www.accredited-times.com/wp-content/uploads/2018/01/Dinner-Party-1.png?fit=2048%2C1112&ssl=1", "Hybrid effects fuel a stimulated mental state that bursts through the body, leaving relaxation in the limbs", "The dense, sticky buds have an intense smell of sweet fruits mixed with pine and eucalyptus trees. The cerebral high comes on quick and leaves consumers feeling uplifted and euphoric while reducing stress and relaxing the mind.","The hefty sativa buds give earthy, sweet coffee flavors that provide a dreamy, cerebral effect." ]
+const description = ["https://www.southernladymagazine.com/wp-content/uploads/2015/05/Picnic1.jpg", "https://i2.wp.com/www.accredited-times.com/wp-content/uploads/2018/01/Dinner-Party-1.png?fit=2048%2C1112&ssl=1","http://assets.kraftfoods.com/recipe_images/opendeploy/56142-aff0a8f08bdcbdab6def9cebe5dda14ff7237760_642x428.jpg", "https://www.sahm.in/cache/860x380-0/wp-content/uploads/2018/06/Weddin_32572resized.jpg","https://dynamix-cdn.s3.amazonaws.com/atlantahurtcom/Tailgating_10-17.jpg" ]
 
-export const getRandomDescription = () => {
+
+
+export const getRandomBeautyShots = () => {
   const randomIndex = Math.floor(Math.random() * description.length)
   return description[randomIndex]
+}
