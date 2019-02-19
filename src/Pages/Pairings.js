871ee3dc839aa429f3ee09d0fc765c1204ffd479
Andy Weiss <andyweiss1982@gmail.com>
2019-02-19 11:59:07 -0500

    resize images and delete package json lock

--- src/Pages/Pairings.js
+++ src/Pages/Pairings.js
@@ -25,17 +25,23 @@ function chooseImage(currentItem) {
 const Pairings = ({ currentItem }) => {
   return(
     <div style={display}>
-      <img src={chooseImage(currentItem)} />
-        <div className = "placement">
-      <h3 className = "name">{currentItem.name}</h3>
-      <div className = "lineage">
-      <h3 className = "parentalLineage">Parental Lineage</h3>
-      {currentItem.lineage && Object.keys(currentItem.lineage).map(key => <div>{key}</div>)}
-      <h3 className = 'subtitle'>Why we like it</h3>  <h3 class= "description">{getRandomDescription()}</h3>
-
+      <img
+        src={chooseImage(currentItem)}
+        width="200px"
+      />
+      <div className = "placement">
+        <h3 className = "name">{currentItem.name}</h3>
+        <div className = "lineage">
+          <h3 className = "parentalLineage">Parental Lineage</h3>
+          {currentItem.lineage && Object.keys(currentItem.lineage).map(key => <div>{key}</div>)}
+          <h3 className = 'subtitle'>Why we like it</h3>  <h3 class= "description">{getRandomDescription()}</h3>
+        </div>
       </div>
-      </div>
-      <img src={getRandomBeautyShots()} alt="beauty shots"></img>
+      <img
+        src={getRandomBeautyShots()}
+        alt="beauty shots"
+        width="200px"
+      />
     </div>
   )
 }
